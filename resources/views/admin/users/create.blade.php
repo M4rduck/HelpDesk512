2c20f3cbb588dc1c0b0d2d5396c5b5c7f76f3dfd
= <=>
2018-09-01 19:49:49 -0500

    Bug en la vista users no deja acomodar los botones

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -87,13 +87,12 @@
                     <!-- /Password -->
                 </div>
             </div>
-            <div class="row col-md-12">
                 <div class="modal-footer">
                 <button type="submit" class="btn btn-primary btn-save" id="bcreate"></button>
                 <button type="button" class="btn btn-danger" data-dismiss="modal">
                 <i class="fa fa-times-circle"></i> Close
                 </button>
-            </div>
+            
             </div>
         </div>
     </div> 
